1c425ed002da364d68d4bb8ff6d29765b2f026e3
buptlj <buptliujie@126.com>
2018-09-04 15:45:45 +0800

    modify the prediction

--- prediction.py
+++ prediction.py
@@ -23,18 +23,22 @@ def pred(test_data, log_dir):
                 break
             file = os.path.join(test_data, f)
             img = cv2.imread(file)
-            img = tf.cast(img, tf.float32)
-            img = model_input.preprocess(img, 224, 224, False)
-            imgs = tf.expand_dims(img, axis=0)
+            image = tf.cast(img, tf.float32)
+            image = model_input.preprocess(image, 224, 224, False)
+            imgs = tf.expand_dims(image, axis=0)
             imgs = imgs.eval()
             pre = sess.run(predict, feed_dict={images: imgs})
-            for p in pre:
-                if np.argmax(p) == 0:
-                    label = 'cat'
-                else:
-                    label = 'dog'
-                print('model:{}, file:{}, label: {}-{} ({:.2f}%)'.
-                      format(ckpt.model_checkpoint_path, file, np.argmax(p), label, np.max(p) * 100))
+
+            if np.argmax(pre[0]) == 0:
+                label = 'cat'
+            else:
+                label = 'dog'
+            print('model:{}, file:{}, label: {}-{} ({:.2f}%)'.
+                  format(ckpt.model_checkpoint_path, file, np.argmax(pre[0]), label, np.max(pre[0]) * 100))
+            text = '{} {}({:.2f}%)'.format(f, label, np.max(pre[0]) * 100)
+            cv2.putText(img, text, (0, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2, cv2.LINE_AA)
+            cv2.imshow('image', img)
+            cv2.waitKey()
             count += 1
 
 
